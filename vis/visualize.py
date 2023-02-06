31239e46c6b48da4a1e5c67424034fb93c3a6650
yinglang <y19941010@126.com>
2023-02-06 09:59:20 +0800

    normal update

--- vis/visualize.py
+++ vis/visualize.py
@@ -4,15 +4,19 @@ import warnings
 from .bbox_utils import *
 
 
-def draw_a_bbox(box, color, linewidth=1, dash=False, fill=False):
+def draw_a_bbox(box, color, linewidth=1, dash=False, fill=False, ax=None):
+    if ax is None:
+        ax = plt.gca()
     if dash:
         # box_to_dashed_rect(plt, box, color, linewidth)
-        plt.axes().add_patch(box_to_rect(box, color, linewidth, fill=fill, ls='--'))
+        ax.add_patch(box_to_rect(box, color, linewidth, fill=fill, ls='--'))
     else:
-        plt.axes().add_patch(box_to_rect(box, color, linewidth, fill=fill))
+        ax.add_patch(box_to_rect(box, color, linewidth, fill=fill))
 
 
-def box_to_dashed_rect(ax, box, color, linewidth=1):
+def box_to_dashed_rect(box, color, linewidth=1, ax=None):
+    if ax is None:
+        ax = plt.gca()
     x1, y1, x2, y2 = box
     ax.plot([x1, x2], [y1, y1], '--', color=color, linewidth=linewidth)
     ax.plot([x1, x2], [y2, y2], '--', color=color, linewidth=linewidth)
@@ -22,10 +26,7 @@ def box_to_dashed_rect(ax, box, color, linewidth=1):
 
 def box_to_rect(box, color, linewidth=1, fill=False, ls='-'):
     """convert an anchor box to a matplotlib rectangle"""
-    if fill:
-        alpha = 0.2
-    else:
-        alpha = None
+    alpha = 0.2 if fill else None
     return plt.Rectangle((box[0], box[1]), box[2]-box[0], box[3]-box[1],
                   fill=fill, alpha=alpha, edgecolor=color, facecolor=color,
                   linewidth=linewidth, linestyle=ls)
@@ -68,7 +69,7 @@ def draw_bbox(fig, bboxes, color=(0, 0, 0), linewidth=1, fontsize=5, normalized_
         if len(box) >= 6 and threshold is not None and box[5] < threshold: continue
         if len(box) >= 5 and class_colors is not None: color = class_colors[int(box[4])]
         if len(box) >= 5 and use_real_line is not None and not use_real_line[int(box[4])]:
-            box_to_dashed_rect(fig, box[:4], color, linewidth)
+            box_to_dashed_rect(box[:4], color, linewidth, ax=fig)
         else:
             rect = box_to_rect(box[:4], color, linewidth)
             fig.add_patch(rect)
@@ -79,3 +80,8 @@ def draw_bbox(fig, bboxes, color=(0, 0, 0), linewidth=1, fontsize=5, normalized_
             if len(box) >= 6: text += " {:.3f}".format(box[5])
             fig.text(box[0], box[1], text,
                      bbox=dict(facecolor=(1, 1, 1), alpha=0.5), fontsize=fontsize, color=(0, 0, 0))
+
+
+def draw_center(ax, bboxes, **kwargs):
+    center = np.array([((x1+x2)/2, (y1+y2)/2)for x1, y1, x2, y2, *arg in bboxes])
+    ax.scatter(center[:, 0], center[:, 1], **kwargs)
